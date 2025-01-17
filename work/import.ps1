73943a5318aaa35bba0290452c681e7766665bcf
Instant Comet <thenightwatchman@163.com>
2025-01-17 21:12:27 +0800

    Update localizations Add export/import script to paratranz

--- work/import.ps1
+++ work/import.ps1
@@ -1,6 +1,6 @@
 # 编辑完成后，将修改后的内容更新回原始JSON
-$updatedJson = Get-Content -Raw "translations.json" | ConvertFrom-Json
-$json = Get-Content -Raw "simplified_chinese/items.json" | ConvertFrom-Json
+$updatedJson = Get-Content -Raw "E:\merchant_localization\work\translations.json" | ConvertFrom-Json
+$json = Get-Content -Raw "E:\merchant_localization\work\items.json" | ConvertFrom-Json
 
 for($i=0; $i -lt $json.Length; $i++) {
     $json[$i].localizedName = $updatedJson[$i].translation
